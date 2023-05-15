bc4376395dca2bed1a9f8b5f69ce01c8bb8fd80d
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-05-15 18:48:10 -0400

    Add mobile menu script

---
+++ src/assets/js/UX/toggle-mobile-menu.js
@@ -0,0 +1,37 @@
+const navbar = document.querySelector("[data-my-navbar]");
+const toggleNavbarMenuButtons = document.querySelectorAll(
+  "[data-button-toggle-navbar-menu]"
+);
+const toggleNavbarMenuLinks = document.querySelectorAll(
+  "[data-links-toggle-navbar-menu]"
+);
+
+const toggleMobileMenu = () => {
+  navbar.classList.toggle("toggle-mobile-menu");
+};
+
+const toggleMobileMenuLinksVisibility = () => {
+  navbar.children[0].classList.toggle("hidden"); //? toggles the ul classlist
+};
+
+const toggleCloseOpenButtons = () => {
+  toggleNavbarMenuButtons.forEach((button) => {
+    button.classList.toggle("hidden");
+  });
+};
+
+toggleNavbarMenuButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    toggleMobileMenu();
+    toggleMobileMenuLinksVisibility();
+    toggleCloseOpenButtons();
+  });
+});
+
+toggleNavbarMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleMobileMenu();
+    toggleMobileMenuLinksVisibility();
+    toggleCloseOpenButtons();
+  });
+});
