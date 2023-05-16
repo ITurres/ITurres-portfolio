b98cda06fc2334a1f8ee12ef0ffa4e148219ff32
iturres <emanueliturres.co@gmail.com>
2023-05-16 19:14:04 -0300

    Add projects 'cards' markup template -functions-

---
+++ src/assets/js/markup-templates/project-card-markup-template.js
@@ -0,0 +1,26 @@
+export const projectCardMarkupTemplate = (...projectsData) => {
+  const projectsDataData = projectsData[0];
+  return `<div class="projects-card">
+    <img src=${projectsDataData.src} alt=${projectsDataData.alt} class='projects-card__img' />
+    <div class='projects-card__title--wrapper'>
+      <h1 class="projects-card__title">${projectsDataData.name1}</h1>
+      <h2 class="projects-card__title">${projectsDataData.name2}</h2>
+    </div>
+    <ul class="projects-card__tech-stack--wrapper" data-tech-stack-holder>
+      <!-- tech-stack will be auto injected -->
+    </ul>
+    <button
+      id="${projectsDataData.id}"
+      class="projects-card__btn my-btn"
+      >See project</
+    button>
+  </div>`;
+};
+
+export const techStackMarkupTemplate = (techStack) => {
+  let currentTechStack = '';
+  techStack.forEach((stackName) => {
+    currentTechStack += `<li class="projects-card__tech-stack">${stackName}</li>`;
+  });
+  return currentTechStack;
+};
