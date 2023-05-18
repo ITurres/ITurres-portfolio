46aaa46626771f494df37cdc48cb47a350e8af11
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-05-18 12:31:38 -0400

    Add Regex to email input validations in js

--- src/assets/js/form-controller/form-validator.js
+++ src/assets/js/form-controller/form-validator.js
@@ -1,6 +1,17 @@
 const formEmailInput = document.querySelector('[data-form-email-input]');
 const formSubmitButton = document.querySelector('[data-form-submit-btn]');
 
+const validateEmailInput = () => {
+  if (
+    !formEmailInput.value.match(
+      /^([a-z\d.!#$%&'*+-/=?^_`{|}~-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/g
+    )
+  ) {
+    return false;
+  }
+  return true;
+};
+
 const addErrorMessageOnDOM = (error) => {
   const contactForm = document.querySelector('[data-contact-form]');
   const errorMessage = document.createElement('p');
