ec80b6227b907cf8a4985f2fa3e4e0bfcd7bf111
iturres <emanueliturres.co@gmail.com>
2023-05-18 19:54:41 -0300

    Add 'localstorage-form-data' -script-

---
+++ src/assets/js/form-controller/localstorage-form-data.js
@@ -0,0 +1,37 @@
+const formObj = {
+  contactFullName: null,
+  contactEmail: null,
+  contactMessage: null
+};
+
+const formInputs = document.querySelectorAll('[data-form-input]');
+
+const storeDataInLocalStorage = (dataKey, dataValue) => {
+  Object.keys(formObj).forEach((key) => {
+    if (dataKey === key) {
+      formObj[key] = dataValue;
+    }
+  });
+  localStorage.setItem('formData', JSON.stringify(formObj));
+};
+
+const loadFormDataFromLocalStorage = () => {
+  const storedFormData = localStorage.getItem('formData');
+  if (storedFormData) {
+    const parsedFormData = JSON.parse(storedFormData);
+    Object.assign(formObj, parsedFormData);
+    formInputs.forEach((input) => {
+      if (input.name in parsedFormData) {
+        input.value = parsedFormData[input.name];
+      }
+    });
+  }
+};
+
+formInputs.forEach((input) => {
+  input.addEventListener('change', (event) => {
+    storeDataInLocalStorage(event.target.name, event.target.value);
+  });
+});
+
+loadFormDataFromLocalStorage();
